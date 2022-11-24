14f12549183db9533882f5d2f8330b832ed7e8a9
fergocen <fbatuhan.ergocen@stu.fsm.edu.tr>
2022-11-24 19:00:56 +0300

    'HW_3'

---
+++ HW_3/script.js
@@ -0,0 +1,23 @@
+async function getSpriteImages() {
+    for (let i = 1; i < 15; i++) {
+        let url = "Sprites/cat (" + i + ").png";
+
+        let options = {
+            method: "GET",
+        };
+
+        let response = await fetch(url, options);
+
+        if (response.status === 200) {
+            let imageBlob = await response.blob();
+            let imageObjectURL = URL.createObjectURL(imageBlob);
+
+            let image = document.createElement("img");
+            image.src = imageObjectURL;
+
+            images.push(image);
+        } else {
+            console.log("HTTP-Error: " + response.status);
+        }
+    }
+}
