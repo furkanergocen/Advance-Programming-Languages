54c3c2b96b205ff7bb2f22d86e8a39d727f9ee7a
fergocen <fbatuhan.ergocen@stu.fsm.edu.tr>
2022-12-23 19:27:57 +0300

    'SW Update'

--- TermProject/sw.js
+++ TermProject/sw.js
@@ -1,5 +1,5 @@
 const CACHE ='TermProject'
-const FILES = ['TermProject.html','back.png','manifest.json','sw.js','./']
+const FILES = ['TermProject.html','main.js','style.css','Icons','back.png','manifest.json','sw.js','./']
 function installCB(e) {
   e.waitUntil(
     caches.open(CACHE)
