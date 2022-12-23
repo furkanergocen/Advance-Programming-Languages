e3502395773786ed747578cf8def426c6da97f61
fergocen <fbatuhan.ergocen@stu.fsm.edu.tr>
2022-12-23 19:11:33 +0300

    'PWA'

---
+++ TermProject/sw.js
@@ -0,0 +1,19 @@
+const CACHE ='TermProject'
+const FILES = ['TermProject.html','back.png','manifest.json','sw.js','./']
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
+function cacheCB(e) {
+    let req = e.request
+    e.respondWith(
+      caches.match(req)
+      .then(r1 => r1 || fetch(req))
+      .catch(console.log)
+    )
+  }
+  self.addEventListener('fetch', cacheCB)
